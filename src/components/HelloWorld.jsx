24c7830526df7590502bd8ea2c0628c6833b3f85
Julián Melero Hidalgo <dev@julianmelero.dev>
2021-07-21 19:20:53 +0200

    First Commit

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function HelloWorld() {
+    return(
+        <div>
+            <h1>Hola Mundo!</h1>
+        </div>
+
+    );
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import  ReactDOM  from 'react';
+
+import HelloWorld from './components/HelloWorld'
+
+
+ReactDOM.Render( <HelloWorld />, document.getElementById('app'));
+
+
