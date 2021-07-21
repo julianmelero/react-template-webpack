ba3189ed7f6e418dba4ade1aaf815507de817ae6
Julián Melero Hidalgo <dev@julianmelero.dev>
2021-07-21 20:00:19 +0200

    webpack dev

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import  ReactDOM  from 'react';
+import  ReactDOM  from 'react-dom';
 
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from './components/HelloWorld';
 
 
-ReactDOM.Render( <HelloWorld />, document.getElementById('app'));
+ReactDOM.render( <HelloWorld />, document.getElementById('app'));
 
 
