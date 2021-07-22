2835d21de01c1025758ea358be0aaff2551fb95b
Julián Melero Hidalgo <dev@julianmelero.dev>
2021-07-22 15:03:12 +0200

    Eslint config

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,55 @@
-const path = require('path');
+const path = require("path");
 
-const htmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = { 
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                    }
-                ]
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: 'html-loader',
-                    }
-                ]
-            },
-            {
-                test: /\.(css|scss)$/,
-                use: [ 
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    "css-loader",
-                    "sass-loader"
-                ]                    
-            }
-        ]
-    },
-    plugins: [
-        new htmlWebpackPlugin({
-            template: './public/index.html',
-            filename: 'index.html',
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'assets/[name].css',
-        })
-    ]
-
-}
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "assets/[name].css",
+    }),
+  ],
+};
