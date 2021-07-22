4430705f8849e40e4d87d956b2537468a3766a76
Julián Melero Hidalgo <dev@julianmelero.dev>
2021-07-22 14:54:08 +0200

    CSS Plugin

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
+import "../assets/styles/App.css";
 
 export default function HelloWorld() {
-    return(
-        <div>
-            <h1>Hola Mundo!</h1>
-        </div>
-
-    );
+  return (
+    <div>
+      <h1>Hola Mundo!</h1>
+    </div>
+  );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const htmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = { 
     entry: './src/index.js',
@@ -29,6 +30,16 @@ module.exports = {
                         loader: 'html-loader',
                     }
                 ]
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [ 
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    "css-loader",
+                    "sass-loader"
+                ]                    
             }
         ]
     },
@@ -37,6 +48,9 @@ module.exports = {
             template: './public/index.html',
             filename: 'index.html',
         }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].css',
+        })
     ]
 
 }
